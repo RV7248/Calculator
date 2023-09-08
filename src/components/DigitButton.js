9358cbe3a118b4b74acc55bbe9ef32014b650062
Rahul Vishwakarma <brightfuture7060@gmail.com>
2023-09-08 14:34:23 +0530

    Initial commit

---
+++ src/components/DigitButton.js
@@ -0,0 +1,6 @@
+// Digit button functions
+import { ACTIONS } from "../App"
+
+export default function DigitButton({ dispatch, digit }) {
+    return <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>{digit}</button>
+}
